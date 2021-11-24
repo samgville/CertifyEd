1f3b0ce646952776e26346ce30bb51c8c7c72567
samgville <sammyvillegas@gmail.com>
2021-11-24 12:43:01 +0100

    Contract function structure and setting files for starting test driven development

--- tests/certificates_unit_tests.py
+++ tests/certificates_unit_tests.py
@@ -1,55 +1,3 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC721";
-
-contract CertificateCreator is ERC721 {
-/**
-  * Store the information about the certificate
-  * @attributes: 
-  * -course_name: Name of the course related to the certificate
-  * -student: Student who took the course
-  * -score: If required the final score of the student who took the course
-  * -certificate_type: Different types of Certifications for Combining, Pricing, etc.
-  * -course_creator: The creator of the course 
-  *  
-  *
- */
-  struct Certificate {
-    string course_name;
-    address student;
-    uint16 score;
-    string certificate_type;
-    address course_creator;
-  }
-
-  /**
-  * Store the information about the certificate
-  * @attributes: 
-  * -course_name: Name of the course related to the certificate
-  * -student: Student who took the course
-  * -score: If required the final score of the student who took the course
-  * -certificate_type: Different types of Certifications for Combining, Pricing, etc.
-  * -course_creator: The creator of the course 
-  *  
-  *
- */
- 
-  constructor() {
-
-  }
-
-  /**
-  * Store the information about the certificate
-  * @attributes: 
-  * -course_name: Name of the course related to the certificate
-  * -student: Student who took the course
-  * -score: If required the final score of the student who took the course
-  * -certificate_type: Different types of Certifications for Combining, Pricing, etc.
-  * -course_creator: The creator of the course 
-  *  
-  *
- */
 
   function createCertificate() public payable {
 
@@ -182,6 +130,3 @@ contract CertificateCreator is ERC721 {
 
   }
 
-
-
-} 
